dea8b31853a66af63cd2d7e2769055d23efba029
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-06-15 11:43:04 -0400

    Add exception logging to CloudWatch in Main method

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,4 +1,6 @@
-﻿using KingmakerDiscordBot.Application.Configuration;
+﻿using Amazon.CloudWatchLogs;
+using Amazon.CloudWatchLogs.Model;
+using KingmakerDiscordBot.Application.Configuration;
 using KingmakerDiscordBot.Application.General;
 using KingmakerDiscordBot.Application.Logic;
 using KingmakerDiscordBot.Application.Observability;
@@ -12,16 +14,53 @@ public static class Program
 
     public static async Task Main(string[] arguments)
     {
-        var configuration = ConfigurationFactory.Create(arguments);
+        try
+        {
+            var configuration = ConfigurationFactory.Create(arguments);
 
-        using var host = Host
-            .CreateDefaultBuilder(arguments)
-            .ConfigureServices(services => services
-                .AddAws(configuration)
-                .AddObservability(configuration)
-                .AddLogic(configuration))
-            .Build();
+            using var host = Host
+                .CreateDefaultBuilder(arguments)
+                .ConfigureServices(services => services
+                    .AddAws(configuration)
+                    .AddObservability(configuration)
+                    .AddLogic(configuration))
+                .Build();
 
-        await host.RunAsync();
+            await host.RunAsync();
+        }
+        catch (Exception exception)
+        {
+            await LogExceptionToCloudWatchAsync(exception);
+        }
+    }
+
+    private static async Task LogExceptionToCloudWatchAsync(Exception exception)
+    {
+        const string logGroupName = "kingmaker-discord-bot";
+        const string logStreamName = "exceptions";
+        using var client = new AmazonCloudWatchLogsClient();
+
+        var logStreamRequest = new CreateLogStreamRequest
+        {
+            LogGroupName = logGroupName,
+            LogStreamName = logStreamName
+        };
+
+        await client.CreateLogStreamAsync(logStreamRequest);
+
+        var logEvent = new InputLogEvent
+        {
+            Message = exception.ToString(),
+            Timestamp = DateTime.UtcNow
+        };
+
+        var eventsRequest = new PutLogEventsRequest
+        {
+            LogGroupName = logGroupName,
+            LogStreamName = logStreamName,
+            LogEvents = [logEvent]
+        };
+
+        await client.PutLogEventsAsync(eventsRequest);
     }
 }
