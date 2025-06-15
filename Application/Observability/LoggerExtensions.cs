e9c0cbc3a28b8e8cdbed26c3631d7b18a5e49cfe
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-06-14 20:43:19 -0400

    Add LoggerExtensions for enhanced logging of Discord.Net messages

---
+++ Application/Observability/LoggerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Microsoft.Extensions.Logging;
+
+namespace KingmakerDiscordBot.Application.Observability;
+
+internal static class LoggerExtensions
+{
+    public static Task Log(this ILogger logger, LogMessage message)
+    {
+        var logLevel = Convert(message.Severity);
+
+        logger.Log(logLevel, message.Exception, "Discord.Net: {Source}: {Message}", message.Source, message.Message);
+        
+        return Task.CompletedTask;
+    }
+
+    private static LogLevel Convert(LogSeverity severity)
+    {
+        return severity switch
+        {
+            LogSeverity.Critical => LogLevel.Critical,
+            LogSeverity.Error => LogLevel.Error,
+            LogSeverity.Warning => LogLevel.Warning,
+            LogSeverity.Info => LogLevel.Information,
+            LogSeverity.Verbose => LogLevel.Debug,
+            LogSeverity.Debug => LogLevel.Trace,
+            _ => throw new ArgumentOutOfRangeException(nameof(severity), severity, $"Unknown {nameof(LogSeverity)}")
+        };
+    }
+}
