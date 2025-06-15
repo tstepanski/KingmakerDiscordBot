673a6fa52ec46e1d6d41be10636d875b627e3ac9
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-06-14 21:50:09 -0400

    Add hosted service Listener to LogicExtensions for improved event handling

--- Application/Logic/LogicExtensions.cs
+++ Application/Logic/LogicExtensions.cs
@@ -14,6 +14,7 @@ internal static class LogicExtensions
         return serviceCollection
             .Configure<InternalDiscordConfiguration>(discordConfiguration)
             .AddSingleton<IAmazonSecretsManager, AmazonSecretsManagerClient>()
-            .AddSingleton<IDiscordClientFactory, DiscordClientFactory>();
+            .AddSingleton<IDiscordClientFactory, DiscordClientFactory>()
+            .AddHostedService<Listener>();
     }
 }
