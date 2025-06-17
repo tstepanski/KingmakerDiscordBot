9e0a57f57b799b5ad8f9d13af7f9d69384296d47
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-06-17 18:47:28 -0400

    Add string extension methods for command formatting and prettification

---
+++ Application/General/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace KingmakerDiscordBot.Application.General;
+
+internal static partial class StringExtensions
+{
+    private static readonly Regex NamePrettifier = NamePrettifierFactory();
+
+    // ReSharper disable once IdentifierTypo
+    public static string Commandify(this string value)
+    {
+        return NamePrettifier.Replace(value, "-$1").ToLower();
+    }
+
+    public static string Prettify(this string value)
+    {
+        return NamePrettifier.Replace(value, " $1");
+    }
+
+    [GeneratedRegex("(?<!^)([A-Z])", RegexOptions.Compiled)]
+    private static partial Regex NamePrettifierFactory();
+}
