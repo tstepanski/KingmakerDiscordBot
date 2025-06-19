f3d27916e0c2a35a3b793a5ff54d488ca2ee7487
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-06-19 14:03:23 -0400

    Refactor IDiscordRestClientProxy to reorganize method declarations for improved clarity and maintainability

--- Application/Discord/IDiscordRestClientProxy.cs
+++ Application/Discord/IDiscordRestClientProxy.cs
@@ -6,29 +6,33 @@ namespace KingmakerDiscordBot.Application.Discord;
 internal interface IDiscordRestClientProxy : IDiscordClient
 {
     bool IsValidHttpInteraction(string publicKey, string signature, string timestamp, string body);
-
-    Task<RestInteraction> ParseHttpInteractionAsync(string publicKey, string signature, string timestamp, string body,
-        Func<InteractionProperties, bool> doApiCallOnCreation);
-
     Task<RestSelfUser> GetCurrentUserAsync(RequestOptions? options = null);
     Task<RestGuildUser> GetCurrentUserGuildMemberAsync(ulong guildId, RequestOptions? options = null);
     Task<RestApplication> GetCurrentBotInfoAsync(RequestOptions? options = null);
-    Task<RestApplication> ModifyCurrentBotApplicationAsync(Action<ModifyApplicationProperties> args,
-        RequestOptions? options = null);
     Task<RestGuildWidget?> GetGuildWidgetAsync(ulong id, RequestOptions? options = null);
     IAsyncEnumerable<IReadOnlyCollection<RestUserGuild>> GetGuildSummariesAsync(RequestOptions? options = null);
     Task<RestGuildUser> GetGuildUserAsync(ulong guildId, ulong id, RequestOptions? options = null);
-    Task<RestGlobalCommand> CreateGlobalCommand(ApplicationCommandProperties properties, RequestOptions? options = null);
-    Task<RestGuildCommand> CreateGuildCommand(ApplicationCommandProperties properties, ulong guildId,
-        RequestOptions? options = null);
-    Task<IReadOnlyCollection<RestGlobalCommand>> GetGlobalApplicationCommands(bool withLocalizations, string locale,
-        RequestOptions? options = null);
     Task DeleteAllGlobalCommandsAsync(RequestOptions? options = null);
     Task AddRoleAsync(ulong guildId, ulong userId, ulong roleId, RequestOptions? options = null);
     Task RemoveRoleAsync(ulong guildId, ulong userId, ulong roleId, RequestOptions? options = null);
     Task AddReactionAsync(ulong channelId, ulong messageId, IEmote emote, RequestOptions? options = null);
     Task LoginAsync(TokenType tokenType, string token, bool validateToken);
     Task LogoutAsync();
+
+    Task<RestInteraction> ParseHttpInteractionAsync(string publicKey, string signature, string timestamp, string body,
+        Func<InteractionProperties, bool> doApiCallOnCreation);
+
+    Task<RestGlobalCommand> CreateGlobalCommand(ApplicationCommandProperties properties, 
+        RequestOptions? options = null);
+    
+    Task<IReadOnlyCollection<RestGlobalCommand>> GetGlobalApplicationCommands(bool withLocalizations, string locale,
+        RequestOptions? options = null);
+
+    Task<RestGuildCommand> CreateGuildCommand(ApplicationCommandProperties properties, ulong guildId,
+        RequestOptions? options = null);
+
+    Task<RestApplication> ModifyCurrentBotApplicationAsync(Action<ModifyApplicationProperties> args,
+        RequestOptions? options = null);
 
     Task<IReadOnlyCollection<RestGuildCommand>> GetGuildApplicationCommands(ulong guildId, bool withLocalizations,
         string locale,
@@ -62,7 +66,7 @@ internal interface IDiscordRestClientProxy : IDiscordClient
 
     Task<RoleConnection> GetUserApplicationRoleConnectionAsync(ulong applicationId, RequestOptions? options = null);
 
-    Task<RoleConnection> ModifyUserApplicationRoleConnectionAsync(ulong applicationId, 
+    Task<RoleConnection> ModifyUserApplicationRoleConnectionAsync(ulong applicationId,
         RoleConnectionProperties roleConnection, RequestOptions? options = null);
 
     LoginState LoginState { get; }
