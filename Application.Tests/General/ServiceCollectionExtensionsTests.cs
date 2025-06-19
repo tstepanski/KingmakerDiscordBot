c09fbc4427b1049dbc10758f98f16336dafabdf4
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-06-19 18:23:48 -0400

    Add unit tests for ServiceCollectionExtensions.RegisterAll method

---
+++ Application.Tests/General/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using KingmakerDiscordBot.Application.General;
+using KingmakerDiscordBot.Application.Listeners.Contracts;
+using Microsoft.Extensions.Configuration;
+
+namespace KingmakerDiscordBot.Application.Tests.General;
+
+public sealed class ServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void RegisterAll_GivenConfiguration_ReturnsIServiceCollection()
+    {
+        var inMemorySettings = new Dictionary<string, string?>
+        {
+            ["AWS:Region"] = "us-east-1"
+        };
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+
+        var serviceCollection = new ServiceCollection();
+        var result = serviceCollection.RegisterAll(configuration);
+
+        Assert.IsType<IServiceCollection>(result, exactMatch: false);
+    }
+    
+    [Fact]
+    public void RegisterAll_GivenConfiguration_Provides1Listener()
+    {
+        var inMemorySettings = new Dictionary<string, string?>
+        {
+            ["AWS:Region"] = "us-east-1"
+        };
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+
+        var serviceProvider = new ServiceCollection()
+            .RegisterAll(configuration)
+            .BuildServiceProvider();
+
+        var result = serviceProvider
+            .GetServices<IListener>()
+            .Count();
+        
+        Assert.Equal(1, result);
+    }
+}
