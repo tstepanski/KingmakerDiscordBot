98fc07c1cd597e611b6e7e9a27a827d3c2f642c8
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-06-22 19:57:22 -0400

    Add logging for application shutdown in Listener class

--- Application/Discord/Listener.cs
+++ Application/Discord/Listener.cs
@@ -1,21 +1,28 @@
 ﻿using KingmakerDiscordBot.Application.Listeners;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace KingmakerDiscordBot.Application.Discord;
 
-internal sealed class Listener(IDiscordClientFactory clientFactory,
-    IListenerRegistrar listenerRegistrar) : BackgroundService
+internal sealed class Listener(IDiscordClientFactory clientFactory, IListenerRegistrar listenerRegistrar, 
+    ILogger<Listener> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var oneMinute = TimeSpan.FromMinutes(1);
         var client = await clientFactory.CreateAsync(stoppingToken);
 
         listenerRegistrar.RegisterAll(client, stoppingToken);
 
         await client.StartAsync();
 
-        await Task.Delay(Timeout.Infinite, stoppingToken);
+        try
+        {
+            await Task.Delay(Timeout.Infinite, stoppingToken);
+        }
+        catch (OperationCanceledException)
+        {
+            logger.LogInformation("Application shutting down, stopping client");
+        }
 
         await client.StopAsync();
     }
