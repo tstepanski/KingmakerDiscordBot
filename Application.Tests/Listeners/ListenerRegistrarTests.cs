9294a0586812e865dc890db4e29ab951b85b2171
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-06-22 19:53:16 -0400

    Add unit tests for ListenerRegistrar to verify GuildAvailabilityListener registration

---
+++ Application.Tests/Listeners/ListenerRegistrarTests.cs
@@ -0,0 +1,36 @@
+﻿using Discord.WebSocket;
+using KingmakerDiscordBot.Application.Discord;
+using KingmakerDiscordBot.Application.Listeners;
+using KingmakerDiscordBot.Application.Listeners.Contracts;
+using Moq;
+
+namespace KingmakerDiscordBot.Application.Tests.Listeners;
+
+public sealed class ListenerRegistrarTests
+{
+    [Fact]
+    public async Task RegisterAll_GivenGuildAvailabilityListener_CorrectlyAttachesInvokeMethod()
+    {
+        var socketClientProxyMock = new Mock<ISocketClientProxy>();
+        var fakeListener = new FakeGuildAvailabilityListener();
+        var registrar = new ListenerRegistrar([fakeListener]);
+        
+        registrar.RegisterAll(socketClientProxyMock.Object, CancellationToken.None);
+
+        await socketClientProxyMock.RaiseAsync(proxy => proxy.GuildAvailable += null, (SocketGuild) null!);
+        
+        Assert.True(fakeListener.Called);
+    }
+    
+    private sealed class FakeGuildAvailabilityListener : IGuildAvailableListener
+    {
+        public bool Called { get; private set; }
+        
+        public Task OnGuildAvailable(IDiscordRestClientProxy _, SocketGuild __, CancellationToken ___)
+        {
+            Called = true;
+            
+            return Task.CompletedTask;
+        }
+    }
+}
