c6b94240108536ad9fcb90e2f581fe9c299f0182
Timothy M. Stepanski <timothy@timothystepanski.com>
2025-06-22 22:06:51 -0400

    removed interactioncreated handler

--- Application/Listeners/DescribeCommandListener.cs
+++ Application/Listeners/DescribeCommandListener.cs
@@ -9,7 +9,7 @@ using KingmakerDiscordBot.Application.StaticData.Commands;
 namespace KingmakerDiscordBot.Application.Listeners;
 
 internal sealed class DescribeCommandListener(ICommandsPayloadGenerator commandsPayloadGenerator, 
-    ILogger<DescribeCommandListener> logger) : ISlashCommandExecutedListener, IInteractionCreatedListener
+    ILogger<DescribeCommandListener> logger) : ISlashCommandExecutedListener
 {
     private readonly ImmutableSortedDictionary<string, IDescribeCommandHandler> _describeCommandHandlers =
         commandsPayloadGenerator
@@ -19,49 +19,31 @@ internal sealed class DescribeCommandListener(ICommandsPayloadGenerator commands
     public async Task OnSlashCommandExecuted(IDiscordRestClientProxy _, ISlashCommandInteraction command,
         CancellationToken cancellationToken = default)
     {
-        try
+        var commandName = command.Data.Name;
+        var json = JsonSerializer.Serialize(command.Data);
+
+        logger.LogInformation("Received {commandName} command from {guildId}: '{payload}'", commandName,
+            command.GuildId, json);
+
+        if (commandName.StartsWith("describe-") is false)
         {
-            var commandName = command.Data.Name;
-            var json = JsonSerializer.Serialize(command.Data);
-
-            logger.LogInformation("Received {commandName} command from {guildId}: '{payload}'", commandName,
-                command.GuildId, json);
-
-            if (commandName.StartsWith("describe-") is false)
-            {
-                return;
-            }
-
-            var handler = _describeCommandHandlers[commandName];
-            var response = handler.GetResponse(command.Data.Options);
-
-            var requestOptions = new RequestOptions
-            {
-                CancelToken = cancellationToken
-            };
-
-            await command.RespondAsync(embed: response, options: requestOptions);
+            return;
         }
-        catch (Exception exception)
+
+        var handler = _describeCommandHandlers[commandName];
+        var response = handler.GetResponse(command.Data.Options);
+
+        var requestOptions = new RequestOptions
         {
-            logger.LogError(exception, "Failed to process command");
-        }
+            CancelToken = cancellationToken
+        };
+
+        await command.RespondAsync(embed: response, options: requestOptions);
     }
 
     Task ISlashCommandExecutedListener.OnSlashCommandExecuted(IDiscordRestClientProxy client, 
         SocketSlashCommand socketSlashCommand, CancellationToken cancellationToken)
     {
         return OnSlashCommandExecuted(client, socketSlashCommand, cancellationToken);
-    }
-
-    async Task IInteractionCreatedListener.OnInteractionCreated(IDiscordRestClientProxy client, 
-        SocketInteraction socketInteraction, CancellationToken cancellationToken)
-    {
-        logger.LogInformation("Received interaction: {name}", socketInteraction.Type);
-        
-        if (socketInteraction is ISlashCommandInteraction command)
-        {
-            await OnSlashCommandExecuted(client, command, cancellationToken);
-        }
     }
 }
